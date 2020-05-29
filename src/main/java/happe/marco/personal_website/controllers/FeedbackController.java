d5528366592cf65b9da078cfd688a61b26a4608f
Marco Happe <marco.happe@icloud.com>
2020-05-29 10:49:37 +0200

    added in email service classes, spring-boot-starter-mail dependency and application.properties

---
+++ src/main/java/happe/marco/personal_website/controllers/FeedbackController.java
@@ -0,0 +1,9 @@
+package happe.marco.personal_website.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FeedbackController {
+
+
+}
---
+++ src/main/java/happe/marco/personal_website/domain/User.java
@@ -0,0 +1,4 @@
+package happe.marco.personal_website.domain;
+
+public class User {
+}
